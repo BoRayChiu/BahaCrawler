54157a5b6cae27d2857446ebc7573ace0ffbb87a
boray <jack43581996@gmail.com>
2023-04-22 18:04:45 +0800

    Demo2

--- baha_crawler.py
+++ baha_crawler.py
@@ -1,6 +1,6 @@
 """This is a crawler for Baha.
 
-It will crawl the data from Baha and generate crawl result.
+It will crawl data from Baha and generate crawl result.
 """
 
 import json
@@ -10,8 +10,8 @@ import requests as rq
 from bs4 import BeautifulSoup as bsp
 
 
-class Crawler:
-    """Crawl data from PTT."""
+class BahaCrawler:
+    """Set basic information for crawling data from Baha."""
 
     def __init__(self):
         self._headers = {
@@ -30,7 +30,7 @@ class Crawler:
         Args:
             url: the website we want to crawl.
         Returns:
-            HTML docs which type is sting.
+            HTML which type is sting.
             For example:
             '<div>Hello!</div>'
         """
@@ -39,29 +39,29 @@ class Crawler:
         return crawl_res
 
 
-class TopicIdCrawler(Crawler):
+class BahaTopicUrlCrawler(BahaCrawler):
     """Get topic urls.
 
-    Inherit from Crawler.
+    Inherit from Baha Crawler.
 
     Attributes:
         board_id: The board we want to crawl.
-        frequency: The number of page we want to get.
+        page: The number of page we want to get.
     """
 
-    def __init__(self, board_id: str, frequency: int):
+    def __init__(self, board_id: str, page: str):
         super().__init__()
         self.__board_id = board_id
-        self.__frequency = frequency
+        self.__frequency = int(page)
     
     @property
-    def result(self):
+    def result(self) -> list:
         """Get topic urls from crawl result and return it.
 
         Returns:
-            A list is formed with topic url.
+            A list is formed with the topic url.
             For example:
-                ['bsn=38898&snA=3980&tnum=1']
+                ['C.php?bsn=38898&snA=4153&tnum=4']
         """
         topic_urls = []
         page = 1
@@ -75,7 +75,7 @@ class TopicIdCrawler(Crawler):
                 )
             )
             res = bsp(self._crawl(url).text.strip(), "html.parser")
-            # Select topic urls.
+            # Select topic url.
             index = res.select(
                 ".b-list__row.b-list-item.b-imglist-item > .b-list__main > a")
             # Append topic url.
@@ -84,47 +84,46 @@ class TopicIdCrawler(Crawler):
             page += 1
         return topic_urls
 
-class ThreadCrawler(Crawler):
-    """Get all content we want in chat thread.
+class BahaTopicCrawler(BahaCrawler):
+    """Get all data we want from chat thread.
 
-    Inherit from Crawler.
+    Inherit from BahaCrawler.
 
     Attributes:
-        board_id: The board we want to crawl.
-        topic_id: The id of topic we wnat to crawl.
+        topic_url: 
+            The url of topic we want to crawl.
+            For example: 
+                "C.php?bsn=60076&snA=7654736"
     """
-    def __init__(self, board_id: str, topic_id: str):
+    def __init__(self, topic_url: str):
         super().__init__()
-        self.__board_id = board_id
-        self.__topic_id = topic_id
+        self.__topic_url = topic_url
 
     @property
-    def result(self):
-        """Get all content we want from crawl result and return it.
+    def result(self) -> list:
+        """Get all data we want from crawl result and return it.
 
         Returns:
             A dicts keys that are category and values are information.
             For example: 
-                {
-                    'Title': 'Hello World!'
-                    'Topics': [
-                        {
-                            'Author': 'abc123', 
-                            'Time': '2023-03-28 00:01:05', 
-                            'Contents': 'Hello World HAHA', 
-                            'Messages': [
-                                {
-                                    'Author': 'cba321', 
-                                    'Time': '2023-03-29 23:51:42', 
-                                    'Contents': 'HAHA'
-                                }
-                            ]
-                        }
-                    ]
-                }
+                [
+                    {
+                        'Url': 
+                        'Title': 'Hello World!'
+                        'Author': 'abc123', 
+                        'Time': '2023-03-28 00:01:05', 
+                        'Contents': 'Hello World HAHA', 
+                        'Messages': [
+                            {
+                                'Author': 'cba321', 
+                                'Time': '2023-03-29 23:51:42', 
+                                'Contents': 'HAHA'
+                            }
+                        ]
+                    }
+                ]
         """
-        thread = {}
-        thread["Topics"] = []
+        topics_box = []
         # Set max_page the smallest number.
         max_page = 1
         page = 1
@@ -132,13 +131,14 @@ class ThreadCrawler(Crawler):
             print("Page"+str(page)+" start!")
             url = "".join(
                 (
-                    "https://forum.gamer.com.tw/C.php?page=",
+                    "https://forum.gamer.com.tw/", 
+                    self.__topic_url[0:6],
+                    "page=",
                     str(page),
-                    "&bsn=",
-                    self.__board_id,
-                    "&snA=",
-                    self.__topic_id
+                    "&",
+                    self.__topic_url[6:]
                 )
+
             )
             # Get html docs.
             res = self._crawl(url)
@@ -146,15 +146,20 @@ class ThreadCrawler(Crawler):
             res = bsp(res.text.strip(), "html.parser")
             # If page is current 1, reset max page.
             if (page == 1):
-                max_page = int(res.select(".BH-pagebtnA > a")[-1].text)
+                pages = res.select(".BH-pagebtnA > a")
+                max_page = int(pages[len(pages)- 1].text)
             # Topics content
             topics = res.select(".c-section__main.c-post")
-            # If page is current 1, store 'Title'
+            # If page is current 1, store Title
             if (page == 1):
-                thread["Title"] = topics[0].select_one(
+                title = topics[0].select_one(
                     ".c-post__header__title").text
             for t in topics:
                 topic = {}
+                # Url
+                topic["Url"] = url
+                # Title
+                topic["Title"] = title
                 # Author
                 topic["Author"] = t.select_one(".userid").text.replace(
                     "\n", "").replace("\xa0", " ")
@@ -170,7 +175,8 @@ class ThreadCrawler(Crawler):
                 if (has_more_messages is not None):
                     message_id = has_more_messages.select_one(
                         ".more-reply")["id"][15:]
-                    topic["Messages"] = self.__crawl_more_messages(message_id)
+                    board_id = self.__topic_url[10:]
+                    topic["Messages"] = self.__crawl_more_messages(board_id, message_id)
                 else:
                     # Message
                     replys = t.select(".c-reply__item")
@@ -182,27 +188,27 @@ class ThreadCrawler(Crawler):
                             ".gamercard")["data-gamercard-userid"]
                         # Time
                         message["Time"] = replys[i].select(".edittime")[
-                            1]["title"][5:]
+                            1]["data-tippy-content"][5:]
                         # Contents
                         message["Contents"] = replys[i].select_one(
                             ".comment_content").text.replace("\n", "").replace("\xa0", " ")
                         messages.append(message)
-                    # Get all Message in 'Messages'
+                    # Get all Message in massages.
                     topic["Messages"] = messages
-                thread["Topics"].append(topic)
+                topics_box.append(topic)
             page += 1
             print("Waiting...")
             # To avoid trouble.
-            time.sleep(10)
+            time.sleep(5)
         print("==========")
-        return thread
+        return topics_box
 
-    def __crawl_more_messages(self, message_id: str):
-        """Get messages if there are more messages button"""
+    def __crawl_more_messages(self, board_id: str, message_id: str):
+        """Get more messages if there are "more messages" button"""
         url = "".join(
             (
                 "https://forum.gamer.com.tw/ajax/moreCommend.php?bsn=",
-                self.__board_id,
+                board_id,
                 "&snB=",
                 message_id,
                 "&returnHtml=1"
@@ -218,9 +224,20 @@ class ThreadCrawler(Crawler):
             message["Author"] = message_bsp.select_one(
                 ".gamercard")["data-gamercard-userid"]
             # Time
-            message["Time"] = message_bsp.select(".edittime")[1]["title"][5:]
+            message["Time"] = message_bsp.select(".edittime")[1]["data-tippy-content"][5:]
             # Contents
             message["Contents"] = message_bsp.select_one(
                 ".comment_content").text.replace("\n", "").replace("\xa0", " ")
             messages.append(message)
         return messages
+
+# How to use:
+if __name__ == "__main__":
+    bu = BahaTopicUrlCrawler("17532", "1")
+    print(bu.result)
+    print("-------------")
+    bc1 = BahaTopicCrawler("C.php?bsn=17532&snA=691826&tnum=2")
+    print(bc1.result)
+    print("-------------")
+    bc2 = BahaTopicCrawler("C.php?bsn=17532&snA=689998&tnum=101")
+    print(bc2.result)
